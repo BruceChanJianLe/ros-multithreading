e42918fe72f9bd4d5c265b0d29f49fe731a61334
brucechanjianle <jianle001@e.ntu.edu.sg>
2021-01-28 09:48:41 +0800

    add single callback multi spinner block header file

---
+++ include/ros-multithreading/single_callback_multi_spinners_block.hpp
@@ -0,0 +1,36 @@
+#ifndef SINGLE_CALLBACK_MULTI_SPINNERS_BLOCK_H_
+#define SINGLE_CALLBACK_MULTI_SPINNERS_BLOCK_H_
+
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+
+namespace multi_threading
+{
+    class ros_node
+    {
+    private:
+        /// @brief ROS private node handle
+        ros::NodeHandle private_nh_;
+
+        /// @brief ROS global node handle
+        ros::NodeHandle global_nh_;
+
+        /// @brief subscriber A
+        ros::Subscriber sub_a_;
+
+        /// @brief subscriber B
+        ros::Subscriber sub_b_;
+
+    public:
+        /// @brief Constructor
+        ros_node();
+
+        /// @brief Destructor
+        ~ros_node();
+
+        /// @brief Run function
+        void run();
+    };
+} // namespace multi_threading
+
+#endif
