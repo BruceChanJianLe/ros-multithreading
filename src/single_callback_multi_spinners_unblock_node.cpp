3364c5ad7b54cde23b2e91c531a46fc89bc849f4
brucechanjianle <jianle001@e.ntu.edu.sg>
2021-01-28 10:17:47 +0800

    add single cb multi spinner unblock node source

---
+++ src/single_callback_multi_spinners_unblock_node.cpp
@@ -0,0 +1,16 @@
+#include "ros-multithreading/single_callback_multi_spinners_unblock.hpp"
+
+
+int main(int argc, char ** argv)
+{
+    // Init ROS node
+    ros::init(argc, argv, "single_callback_multi_spinners_unblock_node");
+
+    // Instantiate node obj
+    multi_threading::ros_node node;
+
+    // Ruun node
+    node.run();
+
+    return 0;
+}
