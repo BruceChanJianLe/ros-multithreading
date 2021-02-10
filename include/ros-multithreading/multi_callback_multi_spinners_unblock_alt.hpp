da626ed4da207c0240d43ffc5e00ab5bf405e6b2
brucechanjianle <jianle001@e.ntu.edu.sg>
2021-02-10 09:39:02 +0800

    add multi cb multi sp unblock alternative header

---
+++ include/ros-multithreading/multi_callback_multi_spinners_unblock_alt.hpp
@@ -0,0 +1,56 @@
+#ifndef MULTI_CALLBACK_MULTI_SPINNERS_UNBLOCK_ALT_H_
+#define MULTI_CALLBACK_MULTI_SPINNERS_UNBLOCK_ALT_H_
+
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+#include <vector>
+
+// ROS multi-threading
+#include <ros/callback_queue.h>
+#include <thread>
+
+namespace multi_threading
+{
+    class ros_node
+    {
+    private:
+        /// @brief ROS private node handle
+        ros::NodeHandle private_nh_;
+
+        /// @brief ROS global node handle
+        ros::NodeHandle global_nh_;
+
+        /// @brief ROS node handle for queue_a
+        ros::NodeHandle nh_a_;
+
+        /// @brief ROS node handle for queue_b
+        ros::NodeHandle nh_b_;
+
+        /// @brief ROS queue
+        ros::CallbackQueue queue_a_;
+
+        /// @brief ROS queue
+        ros::CallbackQueue queue_b_;
+
+        /// @brief Subscriber A
+        ros::Subscriber sub_a_;
+
+        /// @brief Subscriber B
+        ros::Subscriber sub_b_;
+
+        /// @brief Vector of thread
+        std::vector<std::thread> thread_list_;
+
+    public:
+        /// @brief Constructor
+        ros_node();
+
+        /// @brief Destructor
+        ~ros_node();
+
+        /// @brief Run function
+        void run();
+    };
+} // namespace multi_threading
+
+#endif
